1af930954a8be99286ee5063ff3555def0da9237
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-12-20 09:38:34 +0100

    refactor set status, get status message

--- lib/tomato.ex
+++ lib/tomato.ex
@@ -2,7 +2,7 @@ defmodule Tomato.CLI do
   def main(args \\ []) do
     args
     |> parse_args
-    |> set_status
+    |> tomat
   end
 
   defp parse_args(args) do
@@ -16,24 +16,40 @@ defmodule Tomato.CLI do
     opts
   end
 
-  defp get_icon(icon) do
-    if icon, do: icon, else: ":tomato:"
+  defp tomat(opts) do
+    get_status_message(opts) |> set_status
+
+    start_timer(opts[:time])
+
+    "clear" |> set_status
   end
 
-  defp get_description(description) do
-    if description, do: " #{description}", else: " фигачу"
-  end
-
-  defp get_time(time) do
-    if time, do: " до #{Integer.to_string(time)}", else: " до +40"
+  defp set_status(message) do
+    IO.puts(message)
   end
 
   defp get_status_message(opts) do
-    icon = opts[:icon]
-    description = opts[:description]
-    time = opts[:time]
+    Enum.join([
+      get_icon(opts[:icon]),
+      get_description(opts[:description]),
+      get_time(opts[:time])
+    ], " ")
+  end
 
-    get_icon(icon) <> get_description(description) <> get_time(time)
+  defp start_timer(time) do
+    if time, do: start_progress(time), else: start_progress(40)
+  end
+
+  defp get_icon(icon) do
+    if icon, do: ":#{String.downcase(icon)}:", else: ":tomato:"
+  end
+
+  defp get_description(description) do
+    "фигачу" <> if description, do: " #{description}"
+  end
+
+  defp get_time(time) do
+    if time, do: Integer.to_string(time), else: "+40"
   end
 
   defp start_progress(time) do
@@ -43,19 +59,5 @@ defmodule Tomato.CLI do
       ProgressBar.render(i, 100)
       :timer.sleep interval
     end
-  end
-
-  defp start_timer(opts) do
-    time = opts[:time]
-
-    if time, do: start_progress(time), else: start_progress(40)
-  end
-
-  defp set_status(opts) do # сделать перегрузку функции
-    IO.puts get_status_message(opts)
-
-    start_timer(opts)
-
-    IO.puts "clear"
   end
 end
