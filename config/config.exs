35d1c4799c3bf642c8409b6bb94fb86fdcbc0fd8
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-12-22 20:22:38 +0100

    remove mix config from app

--- config/config.exs
+++ config/config.exs
@@ -2,15 +2,6 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :tomato,
-  token: "",
-  emoji: "tomato",
-  text: "working",
-  duration: 40,
-  timezone: "+3",
-  presence: true,
-  sound: "doom.wav"
-
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
