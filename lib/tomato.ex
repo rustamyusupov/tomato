309cc61e0bf6775d309320da832ea19837a4d692
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-12-23 15:19:26 +0100

    show help when non params

--- lib/tomato.ex
+++ lib/tomato.ex
@@ -1,9 +1,3 @@
-# без токена -> сообщение нет токена в конфиге, ссылка на описание в gh
-# без параметров -> сообщение помощь по ключам
-# только время –> сообщение нечего устанавливать
-# комбинации имоджи/текст/присутствие –> устанавливаем без ограничения по времени
-# комбинации имоджи/текст/присутствие + время –> устанавливаем комбинации имоджи/текст/присутствие с ограничением по времени
-
 defmodule Tomato.CLI do
   alias Tomato.Config
   alias Tomato.Slack
@@ -11,10 +5,20 @@ defmodule Tomato.CLI do
 
   @config_filename "./config.json"
   @default_sound "doom.wav"
+  @help [
+    "Tomato is a tool for set slack status and presence\n",
+    "Usage: tomato [parameters]\n",
+    "Parameters:",
+    "  -e - Emoji",
+    "  -t - Text",
+    "  -p - Presence: auto | away",
+    "  -d - Duration: how long set status in minutes"
+  ]
 
   def main(args \\ []) do
     args
     |> parse_args
+    |> Config.init(@config_filename)
     |> process
   end
 
@@ -29,27 +33,18 @@ defmodule Tomato.CLI do
     opts
   end
 
-  defp process ([]) do
-    IO.puts "help"
-  end
-
-  defp process(opts) do
-    params = Config.init(opts, @config_filename)
+  defp process(params) do
     token = params[:token]
     emoji = params[:emoji]
     text = params[:text]
     presence = (params[:presence] == "auto" || params[:presence] == "away") && params[:presence] || false
     duration = params[:duration]
 
-    IO.inspect params
-
     cond do
       !token ->
-        IO.puts "please set token"
+        IO.puts "Token not set -> config.json"
 
       ((emoji || text || presence) && duration) ->
-        IO.puts "emoji|text|presence and duration"
-
         until = get_time(params[:timezone] || 0, duration)
         message = "#{text} #{until}"
         Slack.set_status(token, emoji, message)
@@ -61,15 +56,19 @@ defmodule Tomato.CLI do
         presence && Slack.set_presence(token, !presence)
 
       (emoji || text || presence) ->
-        IO.puts "emoji|text|presence"
-
         Slack.set_status(token, emoji, text)
         presence && Slack.set_presence(token, presence)
 
-      duration
+      duration ->
+        IO.puts "Nothing to do"
+
       true ->
-        IO.puts "nothing to do"
+        show_help()
     end
+  end
+
+  defp show_help do
+    Enum.each(@help, fn line -> IO.puts line end)
   end
 
   defp get_time(timezone, duration) do
