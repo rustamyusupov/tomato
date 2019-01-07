4db834d9ac75124dd2256bf6e6381668b93f9760
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-01-07 21:54:27 +0100

    docs(mix): update app version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tomato.MixProject do
   def project do
     [
       app: :tomato,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       escript: escript(),
